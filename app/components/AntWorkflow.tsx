2c2bfb9b8e43131e3d40ddb64a9484408cb4271e
andrewjaebok <122059823+andrewjaebok@users.noreply.github.com>
2026-07-30 17:29:44 -0700

    Refine ant workflow sequence

--- app/components/AntWorkflow.tsx
+++ app/components/AntWorkflow.tsx
@@ -28,23 +28,19 @@ export default function AntWorkflow() {
             <circle cx="12.8" cy="0" r="2.35" />
             <path d="M4.5-2.2 1-6.3M5.8-1.4 6.2-6.3M4.5 2.2 1 6.3M5.8 1.4 6.2 6.3M9.7-1.8 11.5-5.5M9.7 1.8 11.5 5.5M14.2-1.2 17.6-4.5M14.2 1.2 17.6 4.5" />
           </g>
-          <g id="builder-ant">
-            <use href="#ant-silhouette" />
-            <path className="ant-worn-hat" d="M9.5-3.2h7.1M10.7-3.4c.2-3.4 1.2-5.1 2.5-5.1s2.5 1.7 2.6 5.1Z" />
-          </g>
           <clipPath id="ant-river-clip">
-            <path d="M270-2C262 12 279 24 268 39S278 67 270 84H308C318 67 299 54 310 38S299 12 307-2Z" />
+            <path d="M270-2C262 12 279 24 268 39S278 67 270 84H308C310 68 305 53 309 39C313 25 306 12 307-2Z" />
           </clipPath>
         </defs>
 
         <path className="ant-ground-line" d="M0 56H600" />
 
         <g className="ant-river">
-          <path className="ant-river-bank" d="M270-2C262 12 279 24 268 39S278 67 270 84H308C318 67 299 54 310 38S299 12 307-2Z" />
+          <path className="ant-river-bank" d="M270-2C262 12 279 24 268 39S278 67 270 84H308C310 68 305 53 309 39C313 25 306 12 307-2Z" />
           <g clipPath="url(#ant-river-clip)" className="ant-river-current">
             <path d="M266 6q15 6 46 0M262 22q21 7 53 0M267 39q14 6 44 0M263 56q21 7 52 0M267 73q15 6 45 0" />
           </g>
-          <path className="ant-river-edge" d="M270-2C262 12 279 24 268 39S278 67 270 84M307-2C318 12 299 24 310 38S299 67 308 84" />
+          <path className="ant-river-edge" d="M270-2C262 12 279 24 268 39S278 67 270 84M307-2C306 12 313 25 309 39C305 53 310 68 308 84" />
         </g>
 
         <g className="ant-cycle">
@@ -65,16 +61,6 @@ export default function AntWorkflow() {
             </g>
           </g>
 
-          <g className="ant-blueprint">
-            <rect className="ant-blueprint-paper" x="220" y="62" width="33" height="13" rx="1.5" />
-            <path className="ant-blueprint-sketch" d="M226 70h20m-17 0 3-5h9l3 5m-13-5v5m11-5v5" />
-            <path className="ant-blueprint-roll" d="M252 62v13c5 0 5-13 0-13Z" />
-          </g>
-
-          <g className="ant-hat-cache">
-            <path d="M223 52h7m-6-1c.2-3 1.1-4.2 2.4-4.2s2.2 1.2 2.4 4.2ZM233 52h7m-6-1c.2-3 1.1-4.2 2.4-4.2s2.2 1.2 2.4 4.2ZM243 52h7m-6-1c.2-3 1.1-4.2 2.4-4.2s2.2 1.2 2.4 4.2Z" />
-          </g>
-
           <path className="ant-bridge-shadow" d="M264 54Q288 46 313 54" />
           <g className="ant-bridge">
             <path className="ant-bridge-piece bridge-one" d="M264 53 277 49" />
@@ -84,45 +70,38 @@ export default function AntWorkflow() {
           </g>
 
           <g className="ant-role ant-observer"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-planner"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-resource"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-wait-one"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-wait-two"><use href="#ant-silhouette" /></g>
-
-          <g className="ant-hat-carrier">
-            <g className="carried-hats">
-              <path d="M-5-4h6m-5-1c.2-2.7 1-3.8 2.1-3.8S0-7.7.2-5ZM3-4h6M4-5c.2-2.7 1-3.8 2.1-3.8S8-7.7 8.2-5Z" />
+          <g className="ant-role ant-planner">
+            <use href="#ant-silhouette" />
+            <g className="ant-held-blueprint">
+              <path className="ant-blueprint-grip" d="M14 0 20-1" />
+              <rect className="ant-blueprint-paper" x="19" y="-11" width="32" height="13" rx="1.5" />
+              <path className="ant-blueprint-sketch" d="M24-3h21m-18 0 3-5h9l3 5M30-8v5m10-5v5" />
+              <path className="ant-blueprint-roll" d="M50-11V2c5 0 5-13 0-13Z" />
             </g>
-            <use href="#ant-silhouette" x="-11" y="4" />
           </g>
-
           <g className="ant-builder builder-one">
             <path className="builder-material" d="M-7 0H8" />
-            <use href="#builder-ant" x="-13" y="5" />
+            <use href="#ant-silhouette" x="-13" y="5" />
           </g>
           <g className="ant-builder builder-two">
             <path className="builder-material" d="M-7 0H8" />
-            <use href="#builder-ant" x="-13" y="5" />
+            <use href="#ant-silhouette" x="-13" y="5" />
           </g>
           <g className="ant-builder builder-three">
             <path className="builder-material" d="M-7 0H8" />
-            <use href="#builder-ant" x="-13" y="5" />
+            <use href="#ant-silhouette" x="-13" y="5" />
           </g>
           <g className="ant-builder builder-four">
             <path className="builder-material" d="M-7 0H8" />
-            <use href="#builder-ant" x="-13" y="5" />
+            <use href="#ant-silhouette" x="-13" y="5" />
           </g>
 
           <g className="ant-role ant-validator"><use href="#ant-silhouette" /></g>
 
-          <g className="ant-role ant-colony-one"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-colony-two"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-colony-three"><use href="#ant-silhouette" /></g>
-          <g className="ant-role ant-colony-four"><use href="#ant-silhouette" /></g>
-
-          <g className="ant-return return-one"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
-          <g className="ant-return return-two"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
-          <g className="ant-return return-three"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
+          <g className="ant-role ant-colony ant-colony-one"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
+          <g className="ant-role ant-colony ant-colony-two"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
+          <g className="ant-role ant-colony ant-colony-three"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
+          <g className="ant-role ant-colony ant-colony-four"><circle className="ant-crumb" cx="-7" cy="-5" r="2.1" /><use href="#ant-silhouette" /></g>
         </g>
       </svg>
     </span>
